a8fe5d2bf33075b62d80dca2857af1d3f239e43c
Kate Francisco <kate@example.com>
2026-09-15 00:02:43 +0800

    Improve database environment configuration

--- app/config/database.php
+++ app/config/database.php
@@ -58,13 +58,15 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 $database['main'] = array(
-    'driver' => 'mysql',
-    'hostname' => getenv('DB_HOST'),
-    'port' => getenv('DB_PORT'),
-    'username' => getenv('DB_USERNAME'),
-    'password' => getenv('DB_PASSWORD'),
-    'database' => getenv('DB_NAME'),
+    'driver' => getenv('DB_DRIVER') ?: 'mysql',
+    'hostname' => getenv('DB_HOSTNAME') ?: getenv('DB_HOST') ?: 'localhost',
+    'port' => getenv('DB_PORT') ?: 3306,
+    'username' => getenv('DB_USERNAME') ?: getenv('DB_USER') ?: 'root',
+    'password' => getenv('DB_PASSWORD') ?: '',
+    'database' => getenv('DB_DATABASE') ?: (getenv('DB_NAME') ?: 'mydb'),
     'charset' => 'utf8mb4',
+    'collate' => 'utf8mb4_general_ci',
+    'dbprefix' => '',
 );
-
+$database['default'] = $database['main'];
 ?>
